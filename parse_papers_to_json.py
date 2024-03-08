b00469aa60dcb4af10c8351b123f8dfc7b876656
Sireesh Gururaja <sireesh.gururaja@gmail.com>
2024-03-08 18:07:41 -0500

    Update parser script to dump whole documents

--- parse_papers_to_json.py
+++ parse_papers_to_json.py
@@ -1,17 +1,11 @@
-import itertools
 import json
 import logging
-import math
 import os
 
 import fire
-from matplotlib import pyplot as plt
-import pandas as pd
-import papermage
+
 from papermage import Document
-from papermage.recipes import CoreRecipe
-from papermage.visualizers import plot_entities_on_page
-from tqdm.auto import tqdm
+from papermage_components.materials_recipe import MaterialsRecipe
 
 
 def get_doc_title(document: Document):
@@ -25,29 +19,23 @@ def get_doc_title(document: Document):
 
 
 def parse_papers_to_json(input_folder: str, output_folder: str):
-    recipe = CoreRecipe()
+    recipe = MaterialsRecipe()
 
-    paper_list = [
-        recipe.from_pdf(os.path.join(input_folder, pdf_filename))
+    pdf_list = [
+        pdf_filename
         for pdf_filename in os.listdir(input_folder)
         if pdf_filename.lower().endswith(".pdf")
     ]
-    for paper in paper_list:
-        paper_title = get_doc_title(paper)
-        paper_sentences = [sentence.text.replace("\n", " ") for sentence in paper.sentences]
-        paper_abstract = paper.abstracts[0].text
-        section_text = {}
 
+    paper_list = [
+        recipe.from_pdf(os.path.join(input_folder, pdf_filename)) for pdf_filename in pdf_list
+    ]
+    for paper, pdf_filename in zip(paper_list, pdf_list):
         with open(
-            os.path.join(output_folder, paper_title.replace(" ", "_") + ".json"), "w"
+            os.path.join(output_folder, pdf_filename.lower().replace(".pdf", ".json")), "w"
         ) as f:
             json.dump(
-                {
-                    "title": paper_title,
-                    "abstract": paper_abstract,
-                    "sentences": paper_sentences,
-                    "section_text": section_text,
-                },
+                paper.to_json(),
                 f,
                 indent=4,
             )
