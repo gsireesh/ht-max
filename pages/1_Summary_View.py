61fbde71de0f4ce487365b06fdcdcdef2b414ecd
Sireesh Gururaja <sireesh.gururaja@gmail.com>
2024-07-03 17:42:40 -0400

    Entity type filter on added models

--- pages/1_Summary_View.py
+++ pages/1_Summary_View.py
@@ -1,6 +1,8 @@
-import pandas as pd
+import re
 
+import pandas as pd
 from streamlit.column_config import TextColumn
+from transformers import AutoConfig
 
 from shared_utils import *
 from papermage_components.constants import (
@@ -11,12 +13,34 @@ from papermage_components.constants import (
 st.set_page_config(layout="wide")
 
 
+@st.cache_data
+def get_hf_entity_types(model_name):
+    model_config = AutoConfig.from_pretrained(model_name)
+    model_types = set([re.sub("[BIO]-", "", label) for label in model_config.label2id])
+    return model_types
+
+
+def get_entity_types(model_names):
+    all_entity_types = set()
+    for model_name in model_names:
+        if model_name == "MatIE":
+            all_entity_types.update([e_type for e_type in MAT_IE_TYPES])
+        elif model_name == "GPT-3.5":
+            all_entity_types.update([e_type for e_type in MAT_IE_TYPES])
+        else:
+            all_entity_types.update(get_hf_entity_types(model_name))
+
+    return all_entity_types
+
+
 def get_hf_entities(doc, model_name, allowed_sections, allowed_types):
     all_entities = []
     for section in doc.reading_order_sections:
         if section.metadata["section_name"] not in allowed_sections:
             continue
-        for entity in getattr(section, f"ENTITIES_{model_name}"):
+        for entity in getattr(section, f"ENTITIES_{model_name}", []):
+            if entity.metadata["entity_type"] not in allowed_types:
+                continue
             sentence_context = entity.sentences[0].text
             all_entities.append(
                 {
@@ -100,9 +124,10 @@ def get_tables(doc, filter_string):
 
 
 file_options = os.listdir(PARSED_PAPER_FOLDER)
+show_model_annotations = {}
 
 with st.sidebar:  # .form("File selector"):
-    st.write("Select a pre-parsed file whose results to display")
+    st.write("Select a parsed file whose results to display")
     focus_file = st.session_state.get("focus_document")
     file_selector = st.selectbox(
         "Parsed file",
@@ -112,13 +137,25 @@ with st.sidebar:  # .form("File selector"):
     st.session_state["focus_document"] = file_selector
     focus_document = load_document(file_selector)
 
+    st.write("Show predicted results from:")
+    for model_name in ["MatIE", "GPT-3.5"]:
+        show_model_annotations[model_name] = st.toggle(model_name, value=True)
+    for model_name in st.session_state[CUSTOM_MODELS_KEY]:
+        show_model_annotations[model_name] = st.toggle(model_name, value=False)
+
 
 entities_column, table_column = st.columns([0.5, 0.5])
 with entities_column:
     st.write("## Tagged Entities")
     all_sections = {e.metadata["section_name"] for e in focus_document.reading_order_sections}
+    all_entity_types = get_entity_types(
+        [model_name for model in show_model_annotations if show_model_annotations[model]]
+    )
+
     entity_type_choice = st.multiselect(
-        label="Choose which entity types to display", options=MAT_IE_TYPES, default=MAT_IE_TYPES
+        label="Choose which entity types to display",
+        options=all_entity_types,
+        default=all_entity_types,
     )
     section_choice = st.multiselect(
         label="Choose sections from which to display entities",
@@ -126,25 +163,22 @@ with entities_column:
         default=all_sections,
     )
 
-    show_matie_entities = st.toggle("Show MatIE Entities")
-    show_gpt_entities = st.toggle("Show GPT Entities")
-    show_models = {}
-    for predictor_name in st.session_state.get("custom_models", []):
-        show_models[predictor_name] = st.toggle(f"Show entities from {predictor_name}")
-
     entities = []
-    if show_matie_entities:
+    if show_model_annotations["MatIE"]:
         entities = entities + get_matie_entities(
             focus_document, allowed_sections=section_choice, allowed_types=entity_type_choice
         )
-    if show_gpt_entities:
+    if show_model_annotations["GPT-3.5"]:
         entities = entities + get_gpt_entities(
             focus_document, allowed_sections=section_choice, allowed_types=entity_type_choice
         )
-    for predictor_name, show in show_models.items():
+    for predictor_name, show in show_model_annotations.items():
         if show:
             entities = entities + get_hf_entities(
-                focus_document, predictor_name, allowed_sections=section_choice, allowed_types=None
+                focus_document,
+                predictor_name,
+                allowed_sections=section_choice,
+                allowed_types=entity_type_choice,
             )
 
     st.write(f"Found {len(entities)} entities:")
