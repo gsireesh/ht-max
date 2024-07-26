69201b20c25ceb708422f9eb11990adee591a413
Sireesh Gururaja <sireesh.gururaja@gmail.com>
2024-07-26 16:17:29 -0400

    Update init in interfaces foldr

--- papermage_components/interfaces/__init__.py
+++ papermage_components/interfaces/__init__.py
@@ -1,11 +1,13 @@
-from image_predictor import ImagePredictionResult, ImagePredictorABC
-from text_generation_predictor import (
+from papermage_components.interfaces.image_predictor import ImagePredictionResult, ImagePredictorABC
+from papermage_components.interfaces.text_generation_predictor import (
     TextGenerationPredictorABC,
     LLMMessage,
     LLMValidationResult,
     LLMResults,
 )
-from token_classification_predictor import TokenClassificationPredictorABC
+from papermage_components.interfaces.token_classification_predictor import (
+    TokenClassificationPredictorABC,
+)
 
 __all__ = [
     ImagePredictionResult,
