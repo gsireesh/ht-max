d52600f2c99d8d3ff7a4424e59acb8c586d40833
Sireesh Gururaja <sireesh.gururaja@gmail.com>
2024-08-04 17:47:42 -0400

    Text Generation Docstrings

--- papermage_components/interfaces/text_generation_predictor.py
+++ papermage_components/interfaces/text_generation_predictor.py
@@ -12,28 +12,64 @@ from papermage.predictors import BasePredictor
 
 @dataclass
 class LLMMessage:
+    """
+    Dataclass representing a role and message, in the OpenAI dialogue format.
+    """
+
     role: str
     content: str
 
 
 @dataclass
 class LLMValidationResult:
+    """
+    Class to represent if the LLM predictor is valid, i.e. can make requests.
+    """
+
     is_valid: bool
     failure_message: str
 
 
 @dataclass
 class LLMResults:
+    """
+    Union class for both predicted text and a parsed table for the LLM predictor.
+
+    predicted_text represents the raw predicted text, and results_table is a dict that can have
+    pd.DataFrame() called on it to render it nicely.
+    """
+
     predicted_text: str
     results_table: Optional[dict] = None
 
 
 def get_prompt_generator(prompt_text: str) -> Callable[[str], List[LLMMessage]]:
+    """Returns a function that takes a string and customizes a set of user/system messages to it.
+
+    Parameters
+    ----------
+    prompt_text : The text of the prompt.
+
+    Returns
+    -------
+    A function that, when called with a string, returns a list of system and user messages to prompt
+    an LLM.
+
+    """
     return lambda text: [LLMMessage(role="user", content=f"{prompt_text}\n\n{text}")]
 
 
 class TextGenerationPredictorABC(BasePredictor, ABC):
+    """
+    Interface to implement for text-to-text generation in collage.
+    """
+
     def __init__(self, entity_to_process):
+        """
+        Parameters
+        ----------
+        entity_to_process : represents the PaperMage layer whose entities will be iterated through.
+        """
         self.entity_to_process = entity_to_process
 
     @property
@@ -45,6 +81,7 @@ class TextGenerationPredictorABC(BasePredictor, ABC):
 
     @property
     def predictor_identifier(self) -> str:
+        """MUST IMPLEMENT THIS! Typically, just the name of the underlying model."""
         raise NotImplementedError
 
     @property
@@ -52,9 +89,11 @@ class TextGenerationPredictorABC(BasePredictor, ABC):
         return f"TAGGED_GENERATION_{self.predictor_identifier}"
 
     def generate_from_entity_text(self, entity_text: str) -> str:
+        """MUST IMPLEMENT THIS! Implement the text-to-text function."""
         raise NotImplementedError
 
     def postprocess_text_to_dict(self, text) -> Optional[dict]:
+        """Optional Implementation: if you'd like to do more involved processing on your LLM results."""
         try:
             return json.loads(text)
         except JSONDecodeError:
