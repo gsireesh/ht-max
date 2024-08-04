a4c45c041cedcaf823226cb5e79161b015ef17a6
Sireesh K Gururaja <sgururaj@wqs.cmu.edu>
2024-08-03 20:29:59 -0400

    still wip cde service, but now with some more certainty

--- papermage_components/chem_data_extractor_predictor/chem_data_extractor_service.py
+++ papermage_components/chem_data_extractor_predictor/chem_data_extractor_service.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from ChemDataExtractor import Document
+from chemdataextractor import Document
 import fastapi
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -16,24 +16,30 @@ class EntityCharSpanResponse(BaseModel):
     entity_type: str
 
 
+@app.get("/")
+def get_root():
+    return {"hello": "world"}
+
+
 @app.get("/status")
 def get_status():
     return "Service is up!"
 
 
 @app.post("/annotate_strings")
-def annotate_strings(strings: List[str]) -> List[List[EntityCharSpanResponse]]:
+def annotate_strings(strings: List[str]) -> List[str]:#List[List[EntityCharSpanResponse]]:
     document = Document(*strings)
 
     # compute all mentions before iterating through them
     all_cems = document.cems
-
-    all_entities = []
-    for element in document.elements:
-        element_entities = []
-        for entity in element.cems:
-            entity = EntityCharSpanResponse(text=entity.text, start_char=entity.start,
-                                            end_char=entity.end_char, entity_type="CDE_Chemical")
-            element_entities.append(entity)
-        all_entities.append(element_entities)
-    return all_entities
+    #
+    # all_entities = []
+    # for element in document.elements:
+    #     element_entities = []
+    #     for entity in element.cems:
+    #         entity = EntityCharSpanResponse(text=entity.text, start_char=entity.start,
+    #                                         end_char=entity.end_char, entity_type="CDE_Chemical")
+    #         element_entities.append(entity)
+    #     all_entities.append(element_entities)
+    # return all_entities
+    return strings
