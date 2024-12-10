ccb2c0913303b42b1fa2c566984ba3594867ebed
Sireesh Gururaja <sireesh.gururaja@gmail.com>
2024-12-10 11:53:12 -0500

    fix minor merge error

--- app_config.py
+++ app_config.py
@@ -1,7 +1,7 @@
 import os
 
 
-BASE_CONFIG = {
+app_config = {
     "uploaded_pdf_path": "data/uploaded_papers",
     "processed_paper_path": "data/processed_papers",
     "llm_api_keys": {},
@@ -13,18 +13,14 @@ BASE_CONFIG = {
     "chemdataextractor_service_url": os.environ.get(
         "CHEMDATAEXTRACTOR_SERVICE_URL", "http://localhost:8000"
     ),
-    "matie_service_url": os.environ.get("MATIE_SERVICE_URL", "http://localhost:8003")
+    "matie_service_url": os.environ.get("MATIE_SERVICE_URL", "http://localhost:8003"),
 }
 
 if predefined_config := os.environ.get("CONFIG_NAME"):
     if predefined_config == "sireesh_dev":
         app_config["grobid_url"] = "http://windhoek.sp.cs.cmu.edu:8070"
-        app_config[
-            "chemdataextractor_service_url"
-        ] = "http://windhoek.sp.cs.cmu.edu:8001"
+        app_config["chemdataextractor_service_url"] = "http://windhoek.sp.cs.cmu.edu:8001"
     else:  # 'docker' and everything else
         app_config["grobid_url"] = "http://collage-grobid-1:8070"
-        app_config[
-            "chemdataextractor_service_url"
-        ] = "http://collage-chemdataextractor-1:8000"
+        app_config["chemdataextractor_service_url"] = "http://collage-chemdataextractor-1:8000"
         app_config["matie_service_url"] = "http://collage-matie-1:8000"
